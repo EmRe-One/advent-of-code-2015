ddfc12f5b52ec71515839d398a274b490162ec00
Emre Akgül <emre.akguel.1@gmail.com>
2021-12-04 05:19:31 +0100

    add template-classes

---
+++ src/main/kotlin/de/emreak/adventofcode/days/DayTemplate.kt
@@ -0,0 +1,13 @@
+package de.emreak.adventofcode.days
+
+object DayTemplate {
+
+    fun part1(): Int {
+        return 0
+    }
+
+    fun part2(): Int {
+        return 0
+    }
+
+}
---
+++ src/test/kotlin/de/emreak/adventofcode/days/DayTemplateTest.kt
@@ -0,0 +1,16 @@
+package de.emreak.adventofcode.days
+
+import org.junit.jupiter.api.Test
+import kotlin.test.assertEquals
+
+internal class DayTemplateTest {
+
+    @Test
+    fun part1() {
+    }
+
+    @Test
+    fun part2() {
+    }
+
+}
