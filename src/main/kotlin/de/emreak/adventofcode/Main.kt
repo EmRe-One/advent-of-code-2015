083fe082079be23135eff88354e7e47ebb218850
Emre Akgül <emre.akguel.1@gmail.com>
2021-12-06 05:04:46 +0100

    prepare day3

--- src/main/kotlin/de/emreak/adventofcode/Main.kt
+++ src/main/kotlin/de/emreak/adventofcode/Main.kt
@@ -3,34 +3,51 @@ package de.emreak.adventofcode
 import AdventOfCodeUtils
 import de.emreak.adventofcode.days.Day1
 import de.emreak.adventofcode.days.Day2
+import de.emreak.adventofcode.days.Day3
 
 fun main() {
 
     val day = 2
 
-    when(day) {
-        1 -> {
-            val input: String = AdventOfCodeUtils.readLines(filename = "day1.txt")[0]
-
-            val solution1 = Day1.part1(input)
-            println("Solution1: $solution1")
-
-            val solution2 = Day1.part2(input)
-            println("Solution2: $solution2")
-        }
-        2 -> {
-            val input: List<String> = AdventOfCodeUtils.readLines(filename = "day2.txt")
-
-            val solution1 = Day2.part1(input)
-            println("Solution1: $solution1")
-
-            val solution2 = Day2.part2(input)
-            println("Solution2: $solution2")
-        }
+    when (day) {
+        1 -> solveDay1()
+        2 -> solveDay2()
+        3 -> solveDay3()
+// $1 
         else -> {
             throw IllegalArgumentException("Day $day is not implemented yet.")
         }
     }
-
-
 }
+
+fun solveDay1() {
+    val input: String = AdventOfCodeUtils.readLines(filename = "day1.txt")[0]
+
+    val solution1 = Day1.part1(input)
+    println("Solution1: $solution1")
+
+    val solution2 = Day1.part2(input)
+    println("Solution2: $solution2")
+}
+
+fun solveDay2() {
+    val input: List<String> = AdventOfCodeUtils.readLines(filename = "day2.txt")
+
+    val solution1 = Day2.part1(input)
+    println("Solution1: $solution1")
+
+    val solution2 = Day2.part2(input)
+    println("Solution2: $solution2")
+}
+
+fun solveDay3() {
+    val input = AdventOfCodeUtils.readLines(filename = "day3.txt")
+
+    val solution1 = Day3.part1(input)
+    println("Solution1: $solution1")
+
+    val solution2 = Day3.part2(input)
+    println("Solution2: $solution2")
+}
+
+// $2
